c061bea4120460a26ed5009498bf4348531cf795
David Hoffmann <david.hoffmann@mail.de>
2023-01-20 15:21:01 +0100

    Link from onClick calendar to todo

--- todo-list/src/pages/Calendar.jsx
+++ todo-list/src/pages/Calendar.jsx
@@ -1,15 +1,32 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import { useNavigate } from 'react-router-dom';
 import './Calendar.css'
 
 const CalendarView = () => {
 
     const [value, onChange] = useState(new Date());
+    const navigate = useNavigate();
+    const linkToDay = (value) => {
+        const date = new Date(value);
+        console.log(date.getDate());
+       navigate(`todo/${date.getDate()}`);
 
+      
+    }
+    console.log(new Date());
     return (
         <div className='calendar-container'>
-            <Calendar onChange={onChange} value={value} />
+            <Calendar
+                activeStartDate={new Date('February 1, 2023')}
+                minDate={new Date('February 1, 2023')}
+                maxDate={new Date('February 28, 2023')}
+                defaultView='month'
+                minDetail='month'
+                maxDetail='month'
+                onClickDay={(value, event) => linkToDay(value)}
+            />
         </div>
     )
 };
