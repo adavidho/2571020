7f8b25decb7c0cc9181ff6576dc0f2a6163c031f
David Hoffmann <david.hoffmann@mail.de>
2023-01-22 11:46:30 +0100

    Auto reset input field

--- todo-list/src/pages/ToDo/ToDo.jsx
+++ todo-list/src/pages/ToDo/ToDo.jsx
@@ -1,40 +1,22 @@
-import React from 'react';
+import  React, {useState} from 'react';
 import { useParams } from 'react-router-dom'
 import './ToDo.css'
 import ToDoItem from "./components/ToDoItrem/ToDoItem";
 
 const ToDo = ({ updateDays, toDoStore }) => {
     const {id} = useParams();
-    // let [toDoStore, setToDoStore] = useState({
-    //     1:["clean house", "go fish"], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 
-    //     8:[], 9:[], 10:[], 11:[], 12:[], 13:[], 14:[], 
-    //     15:[], 16:[], 17:[], 18:[], 19:[], 20:[], 21:[],
-    //     22:[], 23:[], 24:[], 25:[], 26:[], 28:[]
-    // });
-   
-    // console.log(toDoStore[id]);
-    // const handleKeyDown = (event) => {
-    //     if (event.key === 'Enter') {
-    //     console.log('do validate')
-    //     setToDoStore(...toDoStore, {id:event.target.value})
-    //     }
-    // }
+    const [newItem, setNewItem] = useState("")
 
-    // const updateDays = (newDay) => {
-    //     let updatedValue = {};
-    //     updatedValue = {id:newDay};
-    //     setToDoStore(toDoStore => ({
-    //          ...toDoStore,
-    //          ...updatedValue
-    //        }));
-    // }
-    
+    const handleChange = event => {
+        setNewItem(event.target.value);
+        };
+
     const addItem = (event) => {
-        if (event.key === 'Enter') {
-            console.log('do validate')
-            let items = toDoStore[id]
-            items.push(event.target.value)
-            updateDays(items)
+        if ((event.key === 'Enter') && !(event.target.value === '')) {
+            let items = toDoStore[id];
+            items.push(event.target.value);
+            updateDays(items);
+            setNewItem("");   
         }
     }
     const rows = [];
@@ -47,7 +29,7 @@ const ToDo = ({ updateDays, toDoStore }) => {
             <h1>To Do List - Day {id}</h1>
             <div className="todo-container">
                 {rows}
-                <input type="text" name={id} onKeyDown={addItem}/>    
+                <input type="text" name={id} value={newItem} onChange={handleChange} onKeyDown={addItem}/>    
             </div>
         </div>
     );
